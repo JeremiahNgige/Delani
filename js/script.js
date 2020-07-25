6b35a3b90235d0fd110f773767ea23f75d33f96d
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-07-25 22:48:01 +0300

    portfolio-imgs with hover effect

--- js/script.js
+++ js/script.js
@@ -11,4 +11,44 @@ $("document").ready(function(){
         $("#prod").toggle();
         $("#prod-descr").toggle();
     }); 
+    $("#kazi4").hover(function(){
+        $(".work4-cont").show();
+    },function(){
+        $(".work4-cont").hide();
+    });
+    $("#kazi3").hover(function(){
+        $(".work3-cont").show();
+    },function(){
+        $(".work3-cont").hide();
+    });
+    $("#kazi2").hover(function(){
+        $(".work2-cont").show();
+    },function(){
+        $(".work2-cont").hide();
+    });
+    $("#kazi1").hover(function(){
+        $(".work1-cont").show();
+    },function(){
+        $(".work1-cont").hide();
+    });
+    $("#kazi5").hover(function(){
+        $(".work5-cont").show();
+    },function(){
+        $(".work5-cont").hide();
+    });
+    $("#kazi6").hover(function(){
+        $(".work6-cont").show();
+    },function(){
+        $(".work6-cont").hide();
+    });
+    $("#kazi7").hover(function(){
+        $(".work7-cont").show();
+    },function(){
+        $(".work7-cont").hide();
+    });
+    $("#kazi8").hover(function(){
+        $(".work8-cont").show();
+    },function(){
+        $(".work8-cont").hide();
+    });
 });
